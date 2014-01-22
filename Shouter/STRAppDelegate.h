d099a784a80712ff6dd9fa82f93dad334778cb3d
Aimee Goffinet <aimeegoffinet@Aimee-Goffinets-MacBook-Pro.local>
2014-01-22 07:19:01 -0500

    Initial Commit

---
+++ Shouter/STRAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  STRAppDelegate.h
+//  Shouter
+//
+//  Created by Aimee Goffinet on 1/22/14.
+//  Copyright (c) 2014 Shouter. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface STRAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Shouter/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Shouter
+//
+//  Created by Aimee Goffinet on 1/22/14.
+//  Copyright (c) 2014 Shouter. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "STRAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([STRAppDelegate class]));
+    }
+}
